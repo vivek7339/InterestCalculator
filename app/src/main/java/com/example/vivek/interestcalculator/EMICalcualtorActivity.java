49bd801bd168838346eefd18c10c42cfa877ce8c
vivek7339 <vivek.7339@gmail.com>
2017-03-30 02:31:16 -0500

    Three Activity Added, UI for SimpleInterestCalculator Almost Complete, Next Implementation of Simple Interest Calculator

---
+++ app/src/main/java/com/example/vivek/interestcalculator/EMICalcualtorActivity.java
@@ -0,0 +1,13 @@
+package com.example.vivek.interestcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EMICalcualtorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emicalcualtor);
+    }
+}
--- app/src/main/java/com/example/vivek/interestcalculator/MainActivity.java
+++ app/src/main/java/com/example/vivek/interestcalculator/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.vivek.interestcalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private Button simpleInterest;
+    private Button compoundInterest;
+    private Button emiCalculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        simpleInterest = (Button) findViewById(R.id.SimpleInterest);
+        compoundInterest = (Button) findViewById(R.id.CompoundInterest);
+        emiCalculator = (Button) findViewById(R.id.EMICalculator);
+
+        simpleInterest.setOnClickListener(this);
+        compoundInterest.setOnClickListener(this);
+        emiCalculator.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == simpleInterest){
+            Intent si = new Intent(this, SimpleInterestCalculatorActivity.class);
+            startActivity(si);
+        }
+        else if(v == compoundInterest){
+            Intent ci = new Intent(this, CompoundIntrestCalcualtorActivity.class);
+            startActivity(ci);
+        }
+        else if(v == emiCalculator){
+            Intent ec = new Intent(this, EMICalcualtorActivity.class);
+            startActivity(ec);
+        }
+
     }
 }
